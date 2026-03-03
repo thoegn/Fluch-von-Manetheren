e7bfb10a64ca8db61f298808c77beab2d8a85df5
Tobias Högn <tobias@hoegn.com>
2026-03-03 14:42:05 +0100

    Quartz sync: Mar 3, 2026, 2:42 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -46,7 +46,7 @@ export const defaultContentPageLayout: PageLayout = {
     drag: true, // whether to allow panning the view around
     zoom: true, // whether to allow zooming in and out
     depth: -1, // how many hops of notes to display
-    scale: 1.1, // default view scale
+    scale: 0.9, // default view scale
     repelForce: 0.5, // how much nodes should repel each other
     centerForce: 0.3, // how much force to use when trying to center the nodes
     linkDistance: 30, // how long should the links be by default?
@@ -61,8 +61,8 @@ export const defaultContentPageLayout: PageLayout = {
     zoom: true,
     depth: -1,
     scale: 0.9,
-    repelForce: 0.5,
-    centerForce: 0.3,
+    repelForce: 16,
+    centerForce: 0.65,
     linkDistance: 30,
     fontSize: 0.6,
     opacityScale: 1,
